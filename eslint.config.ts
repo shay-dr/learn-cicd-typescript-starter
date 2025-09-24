bb4e0a87699855771a1a008d2b467ecab973d6ba
shay-dr <shay.davidr@gmail.com>
2025-09-24 09:30:06 -0600

    updated eslint.config.ts and resolved security errors.

--- eslint.config.ts
+++ eslint.config.ts
@@ -9,10 +9,8 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
     extends: ["js/recommended"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    languageOptions: { globals: globals.browser }, // may need to chagne to globals.node?
+    ignores: ["dist/", "coverage/"],
+    languageOptions: { globals: globals.node },
   },
   tseslint.configs.recommended,
   pluginSecurity.configs.recommended,
